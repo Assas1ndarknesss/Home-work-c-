2f4adccf9e697efacdc79f256e5137c95a666c7b
Assas1ndarkness <assas1ndarknesss@gmail.com>
2025-02-06 15:59:03 +0300

    Array

--- Class.practic/Practic-1/Program.cs
+++ Class.practic/Practic-1/Program.cs
@@ -1,24 +1,37 @@
-﻿int a = Convert.ToInt32(Console.ReadLine());
-int b = Convert.ToInt32(Console.ReadLine());
-int c = Convert.ToChar(Console.ReadLine());
-switch (c)
+﻿// int a = Convert.ToInt32(Console.ReadLine());
+// int b = Convert.ToInt32(Console.ReadLine());
+// int c = Convert.ToChar(Console.ReadLine());
+// switch (c)
+// {
+//     case '+':
+//         System.Console.WriteLine(a+b);
+//         break;
+//     case '-':
+//         System.Console.WriteLine(a-b);
+//         break;
+//     case '*':
+//         System.Console.WriteLine(a*b);
+//         break;
+//     case '/':
+//          System.Console.WriteLine(a/b);
+//          break;
+//     case '%':
+//         System.Console.WriteLine(a%b);
+//         break;
+//     default:
+//         System.Console.WriteLine("Hey man i don't now what you want to do");
+//         break;
+// }
+/////////////////////////////////////////////////////////05.01.2024///////////////////////////////////////////////////////////////////////////////////////////
+
+int cnt = 0;
+for (;;)
 {
-    case '+':
-        System.Console.WriteLine(a+b);
+    int a = Convert.ToInt32(Console.ReadLine());
+    cnt++;
+    if (a==0)
+    {
         break;
-    case '-':
-        System.Console.WriteLine(a-b);
-        break;
-    case '*':
-        System.Console.WriteLine(a*b);
-        break;
-    case '/':
-         System.Console.WriteLine(a/b);
-         break;
-    case '%':
-        System.Console.WriteLine(a%b);
-        break;
-    default:
-        System.Console.WriteLine("Hey man i don't now what you want to do");
-        break;
+    }
 }
+System.Console.WriteLine(cnt);
